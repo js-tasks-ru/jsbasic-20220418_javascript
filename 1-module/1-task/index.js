0eb1d256c5224f0dede8ab4306dbd8b601f628c8
Manifestor51 <manifestorest@gmail.com>
2022-04-18 19:49:12 +0400

    Забрал сразу 2 задачи. Обе проходят тесты

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,22 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0) {
+    return 1;
+  }
+  
+  else if (n < 0) {
+    n = NaN;
+    return n;
+  }
+  else if (n === 1){
+   n = 1;
+   return n;
+  }
+else if (n > 0) {
+  let result = 1;
+  while (n > 0) {   
+   result *= n--;
+  }
+  return (result);
 }
+}
+//console.log(factorial(5));
