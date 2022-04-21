13f838bdecb36f1cea8da9fc1305589bb5a3984e
Manifestor51 <manifestorest@gmail.com>
2022-04-22 01:31:11 +0400

    Задача 3 решена

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,10 @@
 function ucFirst(str) {
-  // ваш код...
+  if (!str) {  
+    return str;
+  } else {
+      return str[0].toUpperCase() + str.slice(1);
+  }
 }
+//ucFirst('вася');
+console.log(ucFirst(''));
+//console.log(str[0]);
