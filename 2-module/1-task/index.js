23f611f32f06d207ddc16389c89833fb15582e1f
Manifestor51 <manifestorest@gmail.com>
2022-04-29 11:42:06 +0400

    Задача решена

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (key in salaries) {
+    if (typeof(salaries[key]) === 'number' && isFinite(salaries[key])) {
+      sum += salaries[key];
+    }
+  }
+return sum;
 }
