95292d5a20ab9d92b921b1da64d5921ea48ed79e
Manifestor51 <manifestorest@gmail.com>
2022-05-05 01:18:25 +0400

    Специально решал не по подсказке в уроке, а так, как я видел, хоть и не оптимально, но я понимаю тут как работает код...

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
   // ваш код...
+  let arr = str.split('');
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] == '-') {
+      arr[i] = arr.splice(i, 0);
+      arr[i + 1] = arr[i + 1].toUpperCase();
+    }
+  }
+  return arr.join('');
 }
