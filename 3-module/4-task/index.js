cedd24fc5590956c6dc6eb9f7d8ff00a159155cf
Manifestor51 <manifestorest@gmail.com>
2022-05-06 01:57:17 +0400

    Задача 3-4 решена

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
   // ваш код...
+  let someUsers = users.filter(item => item.age <= age);
+  let str = '';
+  for (let i = 0; i < someUsers.length; i++) {
+    str += `${someUsers[i].name}, ${someUsers[i].balance}\n`;
+  }
+  return str.substring(0, str.length - 1);
 }
