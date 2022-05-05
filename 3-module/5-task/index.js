b44737c7151446f557ace66b9186a6de1b8c7206
Manifestor51 <manifestorest@gmail.com>
2022-05-06 00:04:01 +0400

    Задача 3-5 решена

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,15 @@
 function getMinMax(str) {
-  // ваш код...
+  // ваш код... 
+  str = str.split(' ');
+  for (let i = 0; i < str.length; i++) {
+    str[i] = +str[i];
+    if (str[i] !== str[i]) {
+      str[i] = str.splice(i + 1, 1);
+    }
+  }
+  let result = {
+  min: Math.min(...str),
+  max: Math.max(...str),
+  }
+return result;
 }
