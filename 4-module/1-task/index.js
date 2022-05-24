254591d565c1e42d6a9d7549125a7c81a31b44d3
Manifestor51 <manifestorest@gmail.com>
2022-05-24 11:28:57 +0400

    просит изменить код...

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,14 +1,3 @@
-/* function makeFriendsList(friends) {
-  let ul = document.createElement('UL');
-  document.body.appendChild(ul);
-  for (let user of friends) {
-    let li = `<li>${user.firstName} ${user.lastName}</li>`;
-    document.querySelector('UL').insertAdjacentHTML('beforeEnd', li);
-    }
-    return ul;
-  }
-  */
-
   function makeFriendsList(friends) {
     const ul = document.createElement('ul');
   
